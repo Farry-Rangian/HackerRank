feba5e726d4334a970ba2bfd819cbbe23ff863d6
Farry-Rangian <farry12angian@gmail.com>
2021-02-07 11:55:35 +0800

    first commit

---
+++ C++/01-input-&-output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    int n,j,k; 
+    cin >> n>>j>>k;
+    cout<<n+j+k<<endl;
+    return 0;
+}
